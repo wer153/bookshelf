4bbafc5737358c0828ad089d5d6f59101a5cdbf9
Kim Minki <wer153@naver.com>
2023-11-14 23:09:52 +0900

    feature: add book api

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from litestar import Litestar, get
+from litestar import Litestar, get, post
 
 
 @get("/healthcheck")
 async def healthcheck() -> str:
     return "healthy"
+
+
+@post("/books")
+async def add_book(isbn: str) -> str:
+    return isbn
 
 
 app = Litestar([healthcheck])
